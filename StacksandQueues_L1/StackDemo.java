64893d031ccbaa7ddfe19cd14423c01c67a4ba86
DTG-J <grozdanovdt@abv.bg>
2023-10-18 14:10:44 +0300

    Create StackDemo.java

---
+++ StacksandQueues_L1/StackDemo.java
@@ -0,0 +1,43 @@
+package StacksandQueues_L1;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque <Integer> stack = new ArrayDeque<>();
+
+        //1. Stack fill
+       // Arrays.stream(scanner.nextLine().split("\\s+")).map (Integer::parseInt).forEach(stack::push);
+
+
+
+        stack.push(2);
+        stack.push(3);
+        stack.push(5);
+        stack.push(13);
+
+        //2. Stack print
+        System.out.println(stack.stream().map (String::valueOf).collect(Collectors.joining(", ")));
+
+        System.out.println(stack.peek());
+
+        int lastElement = stack.pop();
+        System.out.println(lastElement);
+
+        System.out.println(stack.size()); //3
+        System.out.println(stack.contains(3)); //true
+        System.out.println(stack.contains(32)); //false
+        System.out.println(stack.isEmpty()); //false
+
+        stack.forEach(System.out::println);
+
+
+
+
+
+    }
+}
