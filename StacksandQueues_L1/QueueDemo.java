87c8bf8abdb2d26a98b4c1571fb2f28481d1beb3
DTG-J <grozdanovdt@abv.bg>
2023-10-18 16:00:11 +0300

    Update QueueDemo.java

--- StacksandQueues_L1/QueueDemo.java
+++ StacksandQueues_L1/QueueDemo.java
@@ -11,7 +11,7 @@ public class QueueDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<Integer> queue1 = new ArrayDeque<>();
+        //ArrayDeque<Integer> queue1 = new ArrayDeque<>();
         //ArrayDeque<String> queue2 = new ArrayDeque<>();
 
         //1. Пълнене на опашка от конзолата int:
@@ -20,8 +20,8 @@ public class QueueDemo {
 
          //2.  Пълнене на опашка от конзолата String:
 
-        Deque<String> deque = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .collect(Collectors.toCollection(ArrayDeque::new));
+        /*Deque<String> deque = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .collect(Collectors.toCollection(ArrayDeque::new));*/
 
         /*3. Отпечатване на данни от опашката:*/
 
@@ -34,8 +34,30 @@ public class QueueDemo {
         /*deque.forEach(System.out::print);*/
 
         //Трети начин
-        System.out.println(deque.stream().map(String::valueOf)
-                .collect(Collectors.joining(", ")));
+      /*  System.out.println(deque.stream().map(String::valueOf)
+                .collect(Collectors.joining(", ")));*/
+
+
+       /* 4. Additional operations: */
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        queue.add(2);
+        queue.offer(3);
+        queue.add(5);
+        queue.offer(13);
+
+        //System.out.println(queue.peek()); //2
+
+        int firstElement = queue.remove(); //2
+        System.out.println(firstElement);
+
+        System.out.println(queue.size()); //2
+        System.out.println(queue.contains(3)); //true
+        System.out.println(queue.contains(32)); //false
+        System.out.println(queue.isEmpty()); //false
+
+        queue.forEach(System.out::println);
+
+
 
 
     }
