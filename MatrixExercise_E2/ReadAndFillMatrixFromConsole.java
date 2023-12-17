6b7127aa2ec92f67035649b48dee512031f0fff4
DTG-J <grozdanovdt@abv.bg>
2023-12-17 11:31:46 +0200

    Update ReadAndFillMatrixFromConsole.java

--- MatrixExercise_E2/ReadAndFillMatrixFromConsole.java
+++ MatrixExercise_E2/ReadAndFillMatrixFromConsole.java
@@ -25,4 +25,16 @@ public class ReadAndFillMatrixFromConsole {
         }
 
     }
+    //input shows that if we receive more columns from the console than we expext in the code -> in printing we
+    //receive the number of expected columns filled with numbers !!!!
+    //input from the console
+    //18 19 20 21 22 23 24 25
+    //26 27 28 29 30 31 32 33
+    //34 35 36 37 38 39 40 41
+    //42 43 44 45 46 47 48 49
+    //Printed result:
+    //18 19 20 21 22 23 24
+    //26 27 28 29 30 31 32
+    //34 35 36 37 38 39 40
+    //42 43 44 45 46 47 48
 }
