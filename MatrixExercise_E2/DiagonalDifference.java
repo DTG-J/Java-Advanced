a31eef99620e5b505e3f7c96ee1fad93185497c7
DTG-J <grozdanovdt@abv.bg>
2023-12-19 20:00:17 +0200

    Update DiagonalDifference.java

--- MatrixExercise_E2/DiagonalDifference.java
+++ MatrixExercise_E2/DiagonalDifference.java
@@ -6,24 +6,37 @@ import java.util.Scanner;
 public class DiagonalDifference {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
+        int matrixSize = Integer.parseInt(scanner.nextLine());
 
-        int [][] matrix = new int[n][n];
+        int [][] matrix = new int[matrixSize][matrixSize];
 
-        for (int row = 0; row < n; row++) {
+        MatrixConsoleRead(matrixSize, scanner, matrix);
+
+        int mainDiagSum = 0;
+        int secondDiagSum = 0;
+        for (int counter = 0; counter < matrix.length; counter++) {
+            mainDiagSum += matrix [counter][counter];
+        }
+        /*for (int row = 0; row < matrixSize; row++) {
+            for (int col = 0; col < matrixSize; col++) {
+                if (row == col){
+                    mainDiagSum += matrix [row][col];
+                }
+            }
+        }*/
+        MatrixConsoleRead(matrixSize, scanner, matrix);
+        for (int i = matrixSize -1; i >=0 ; i--) {
+            secondDiagSum += matrix [i][i];
+        }
+        System.out.println();
+    }
+
+    private static void MatrixConsoleRead(int matrixSize, Scanner scanner, int[][] matrix) {
+        for (int row = 0; row < matrixSize; row++) {
             int [] input = Arrays.stream(scanner.nextLine().split(" "))
                     .mapToInt(Integer::parseInt).toArray();
 
-            matrix [row] = input;
+            matrix[row] = input;
         }
-
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            if ()
-            sum += i;
-
-        }
-
     }
-
 }
