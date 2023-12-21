637283ce7b7cb57df0f0388f232c38ac7573ba14
DTG-J <grozdanovdt@abv.bg>
2023-12-21 19:01:47 +0200

    Create MaximalSum.java

---
+++ MatrixExercise_E2/MaximalSum.java
@@ -0,0 +1,16 @@
+package MultidimentionalArrays_Matrix_L2.src;
+
+import java.util.Scanner;
+
+public class MaximalSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    String [] input = scanner.nextLine().split(" ");
+    int rows = Integer.parseInt(input [0]);
+    int cols = Integer.parseInt(input[1]);
+
+    
+
+
+    }
+}
