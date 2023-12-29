d3bf6ae3ab9dba15f3e6ae27a78d97a7c9c62fff
DTG-J <grozdanovdt@abv.bg>
2023-12-29 09:32:35 +0200

    Create FishingCompetition.java

---
+++ MatrixExercise_E2/FishingCompetition.java
@@ -0,0 +1,11 @@
+package MultidimentionalArrays_Matrix_L2.src;
+
+import java.util.Scanner;
+
+public class FishingCompetition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        
+    }
+}
