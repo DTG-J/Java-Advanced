8c8905a2e77711edf01bdb54c88fe9c6e90c9b08
DTG-J <grozdanovdt@abv.bg>
2024-01-07 18:36:06 +0200

    Create Kindergarten.java

---
+++ Clasess and Methods/Kindergarten/Kindergarten.java
@@ -0,0 +1,33 @@
+package kindergarten;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Kindergarten {
+    //•	name: String
+    //•	capacity: int
+    //•	registry: List<Child>
+    private String name;
+    private  int capacity;
+    private List<Child> registry;
+
+    public Kindergarten(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.registry = new ArrayList<>();
+    }
+    //•	Method	 addChild(Child child) -
+    // Adds a child to the registry if there is room for it and returns true.
+    // If there is no room for another child, returns false.
+    public boolean addChild(Child child){
+        if (capacity > registry.size()) {
+           this.registry.add(child);
+            return true;
+        }
+        return false;
+    }
+
+}
