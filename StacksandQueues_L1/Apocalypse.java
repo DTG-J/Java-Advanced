fabf0a8d4614e5c13246a1a25d6737b86215c56a
DTG-J <grozdanovdt@abv.bg>
2024-01-08 18:50:10 +0200

    Update Apocalypse.java

--- StacksandQueues_L1/Apocalypse.java
+++ StacksandQueues_L1/Apocalypse.java
@@ -1,7 +1,23 @@
 package StacksandQueues_L1;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class Apocalypse {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<String> queue = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .forEach(queue::offer);
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .forEach(stack::push);
+        System.out.println();
     }
 }
